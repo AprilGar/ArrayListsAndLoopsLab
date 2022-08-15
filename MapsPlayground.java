ad22a62513476e3f420f31fa697233d752f56d9a
April Garlington <april.garlington@hotmail.co.uk>
2022-08-15 20:26:39 +0100

    work on maps

--- MapsPlayground.java
+++ MapsPlayground.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class MapsPlayground {
+import java.util.HashMap;
+
+public class MapsPlayground {
+
+    public static void main(String[] args) {
+
+        HashMap<String, Integer> trainerEmploymentLengths = new HashMap<>();
+        //Maps store data in key-value pairs
+        trainerEmploymentLengths.put("Colin", 11);
+        trainerEmploymentLengths.put("Anna", 4);
+        trainerEmploymentLengths.put("Iain", 9);
+
+        //use .get() method to retrieve data
+        int annaEmploymentLength = trainerEmploymentLengths.get("Anna");
+        System.out.println(annaEmploymentLength);
+
+        trainerEmploymentLengths.replace("Colin", 12);
+        System.out.println(trainerEmploymentLengths.get("Colin"));
+    }
 }
